693b1592de6fcdb86c56ecbb5fa5f69d82237147
Darshan Rai <darshanjrai@gmail.com>
2015-03-14 22:37:40 +0530

    Refactor application_controller -- calculating total due for current_user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,14 +2,20 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  before_action :calculate_total_due, if: :user_signed_in?
+  before_action :set_total_due, if: :user_signed_in?
 
   private
-    def calculate_total_due
+
+    def set_total_due
+      @total_due = calculate_total_due(current_user)
+    end
+
+    def calculate_total_due(user)
       sum = 0
-      EventRegistration.where(user: current_user).each do |registration|
-        sum += registration.amount
+      EventRegistration.where(user: current_user).each do |r|
+        sum += r.amount
       end
-      @total_due = sum
+
+      return sum
     end
 end
